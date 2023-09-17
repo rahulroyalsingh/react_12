b2aa8f4d88e8a37c479d49132b84b142a50a3513
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-09-18 01:27:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './Student'
+
+function App()
+{
+   return (
+
+  <div className="App">
+  <h1>Props !</h1>
+     <Student />
+    </div>
+   );
+}
+export default App;
---
+++ Student.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Students extends React.Component{
+   render()
+   {
+      return(
+        <div>Student</div>
+     )
+   }
+}
